d1e2577a60ab20a8944d86f7d9ff3c9387b097c5
faolanbig <faolan.big@web.de>
2026-02-18 17:17:11 +0100

    adding basic init

--- OpenGL_TestBench_Windows/main.cpp
+++ OpenGL_TestBench_Windows/main.cpp
@@ -4,5 +4,28 @@
 
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Test Bench", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	return 0;
 }
