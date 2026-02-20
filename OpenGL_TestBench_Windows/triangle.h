a3c9af91eef75cad943d9504c1540df1eb161c52
faolanbig <faolan.big@web.de>
2026-02-20 01:04:14 +0100

    update

---
+++ OpenGL_TestBench_Windows/triangle.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/*float vertices[] = {
+	// positions         // colors
+	 0.0f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // top vertex (red)
+	 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom right vertex (green)
+	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f  // bottom left vertex (blue)
+};*/
+
+float vertices[] = {
+	 0.0f,  0.5f, 0.0f, // top vertex
+	 0.5f, -0.5f, 0.0f, // bottom right vertex
+	-0.5f, -0.5f, 0.0f  // bottom left vertex
+};
